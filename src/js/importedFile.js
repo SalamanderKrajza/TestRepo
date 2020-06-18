bc02553bcea9526b7db7eafddea46ffc3155a954
Aleksander Krysa <salamanderkrajza@gmail.com>
2020-06-18 20:49:00 +0200

    Example of executing .js file inside other .js file

---
+++ src/js/importedFile.js
@@ -0,0 +1,6 @@
+console.log ('This is some example text from iported file') // some console log
+document.querySelector('body').style.backgroundColor = "#f0d" //Change BG color
+console.log ('This text is after changing BG color') // some console log
+document.querySelector('.hello__logo').src = "http://www.pngmart.com/files/11/Sad-Pepe-The-Frog-PNG-Transparent.png" // change logo
+document.querySelector('.hello__logo').style.width = '200px' // Change size
+console.log ('Another console log, after changing logo')
--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,13 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('HELLO 🚀');
+
+import {} from './importedFile.js' 
+
+console.log ("file: importedFile.js executed once!")
+
+//I am adding this another time to show that previous import is not executed another time if i am import this
+import {} from './importedFile.js' 
+
+console.log("this is last console log (we can see that between first execution and this line there is no console logs")
