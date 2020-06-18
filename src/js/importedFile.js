0f956c20d5322bf2aa9b69e300324f425e09d263
Aleksander Krysa <salamanderkrajza@gmail.com>
2020-06-18 21:50:39 +0200

    Import as name

--- src/js/importedFile.js
+++ src/js/importedFile.js
@@ -4,3 +4,6 @@ console.log ('This text is after changing BG color') // some console log
 document.querySelector('.hello__logo').src = "http://www.pngmart.com/files/11/Sad-Pepe-The-Frog-PNG-Transparent.png" // change logo
 document.querySelector('.hello__logo').style.width = '200px' // Change size
 console.log ('Another console log, after changing logo')
+
+const something = "something from other file"
+export {something}; 
--- src/js/index.js
+++ src/js/index.js
@@ -8,11 +8,13 @@ import '../scss/main.scss';
 
 console.log('HELLO 🚀');
 
-import {} from './importedFile.js' 
+import {something} from './importedFile.js' ;
 
 console.log ("file: importedFile.js executed once!")
 
 //I am adding this another time to show that previous import is not executed another time if i am import this
-import {} from './importedFile.js' 
+//import {something} from './importedFile.js' ; // Now commented because we cant import the same name twice
 
 console.log("this is last console log (we can see that between first execution and this line there is no console logs")
+
+console.log(`${something}`);
